8aa03ed04f75b993801b2b262c1e7fe8e7aa2d11
basha-github <sharifabee.alur@gmail.com>
2024-09-19 14:23:43 +0530

    update =

--- NumberOfNotes.java
+++ NumberOfNotes.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class NumberOfNotes {
 
@@ -9,32 +10,41 @@ public class NumberOfNotes {
 		// 5---1
 		// 2---1
 		// 1--1
+		// 9384720293
+		
 		int money=51, notes20 =0,notes10=0,
 				notes5=0,notes2=0,notes1=0,
 						notes50=0;
 		String totalValue = "";
 		
-		if(money > 50) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter money");
+		money = sc.nextInt();
+		
+		
+		
+		
+		if(money >= 50) {
 			notes50 = money/50;
 			money = money - (notes50 * 50);
 			totalValue = totalValue + "50Notes--->"+notes50+"\n";
 		}
-		if(money > 20) {
+		if(money >= 20) {
 			notes20 = money/20;
 			money = money - (notes20 * 20);
 			totalValue = totalValue +"20Notes--->"+notes20+"\n";
 		}
-		if(money > 10) {
+		if(money >= 10) {
 			notes10 = money/10;
 			money = money - (notes10 * 10);
 			totalValue = totalValue +"10Notes--->"+notes10+"\n";
 		}
-		if(money > 5) {
+		if(money >= 5) {
 			notes5 = money/5;
 			money = money - (notes5 * 5);
 			totalValue = totalValue +"5Notes--->"+notes5+"\n";
 		}
-		if(money > 2) {
+		if(money >= 2) {
 			notes2 = money/2;
 			money = money - (notes2 * 2);
 			totalValue = totalValue +"2 Coins--->"+notes2+"\n";

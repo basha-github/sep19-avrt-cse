ac0ceb4c410f5f46ef5c52186458fa78d5197448
basha-github <sharifabee.alur@gmail.com>
2024-09-19 14:13:14 +0530

    first commit

---
+++ NumberOfNotes.java
@@ -0,0 +1,57 @@
+
+public class NumberOfNotes {
+
+	public static void main(String[] args) {
+		//Write a program to count total number of notes
+		//in given amount
+		// 68
+		// 20 --1
+		// 5---1
+		// 2---1
+		// 1--1
+		int money=51, notes20 =0,notes10=0,
+				notes5=0,notes2=0,notes1=0,
+						notes50=0;
+		String totalValue = "";
+		
+		if(money > 50) {
+			notes50 = money/50;
+			money = money - (notes50 * 50);
+			totalValue = totalValue + "50Notes--->"+notes50+"\n";
+		}
+		if(money > 20) {
+			notes20 = money/20;
+			money = money - (notes20 * 20);
+			totalValue = totalValue +"20Notes--->"+notes20+"\n";
+		}
+		if(money > 10) {
+			notes10 = money/10;
+			money = money - (notes10 * 10);
+			totalValue = totalValue +"10Notes--->"+notes10+"\n";
+		}
+		if(money > 5) {
+			notes5 = money/5;
+			money = money - (notes5 * 5);
+			totalValue = totalValue +"5Notes--->"+notes5+"\n";
+		}
+		if(money > 2) {
+			notes2 = money/2;
+			money = money - (notes2 * 2);
+			totalValue = totalValue +"2 Coins--->"+notes2+"\n";
+		}
+		if(money ==1) {
+			notes1 = money;
+			money = money - notes1 ;
+			totalValue = totalValue +"1 Coin--->"+notes1;
+		}
+		
+		System.out.println(totalValue);
+		
+		
+		
+		
+		
+	}
+	
+
+}
